e43593d3d2df504fa12840156a0fa7e8d21f6b41
13 <keybladeu@gmail.com>
2023-10-14 04:51:33 +0800

    refactor: Optimized Hitokoto code

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // Hitokoto
-var httpRequest = new XMLHttpRequest();
-    var url = "https://v1.hitokoto.cn?encode=json";
-    httpRequest.open('GET', url, true);
-    httpRequest.send();
-    httpRequest.onreadystatechange = function () {
-        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-            var json = JSON.parse(httpRequest.responseText);
+fetch("https://v1.hitokoto.cn?encode=json")
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            document.getElementById("hitokoto").innerHTML = "Hitokoto加载失败惹";
+        }
+    })
+    .then(json => {
+        if (json) {
             function replaceNullWithEmpty(obj) {    // 遍历 JSON 将 NULL 替换为空值
                 for (let key in obj) {
                     if (obj[key] === null) {
                         obj[key] = "";
-                    } else if (typeof obj[key] === 'object') {
-                        replaceNullWithEmpty(obj[key]);
                     }
                 }
             }
@@ -21,4 +22,4 @@ var httpRequest = new XMLHttpRequest();
             document.getElementById("hitokoto").target = "_blank";
             document.getElementById("hitokoto").title = "——" + json.from_who + "「" + json.from + "」";
         }
-    };
+    });
